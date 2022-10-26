e47e84a5645f8bbbf424416f57854114fff416bc
Luka De Bakker <lukadebakker5@gmail.com>
2022-10-26 22:12:35 +0200

    ✨ Started testing out how to do the API calls.

--- app/lib/mangaDex/api_client.dart
+++ app/lib/mangaDex/api_client.dart
@@ -3,7 +3,12 @@ import 'package:dio/dio.dart';
 class ApiClient {
   final Dio dio = Dio();
 
-  Future<Response> Login() async {}
+  Future<Response> Login() async {
+    try {} on DioError catch (e) {
+      //returns the error object if there is any.
+      return e.response!.data;
+    }
+  }
 
   Future<Response> Register() async {}
 }
