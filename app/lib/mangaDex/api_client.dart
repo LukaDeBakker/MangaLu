87df4c01511f38c233a5690f32702d502a9bf41d
Luka De Bakker <lukadebakker5@gmail.com>
2022-10-27 21:22:43 +0200

    ✨ Wrote my first API call!

--- app/lib/mangaDex/api_client.dart
+++ app/lib/mangaDex/api_client.dart
@@ -1,14 +1,22 @@
 import 'package:dio/dio.dart';
 
 class ApiClient {
-  final Dio dio = Dio();
+  final Dio _dio = Dio();
+  final apiUrl = "https://api.mangadex.org";
 
-  Future<Response> Login() async {
-    try {} on DioError catch (e) {
+  Future<Response> Login(String username, String password) async {
+    try {
+      Response response = await _dio.post("$apiUrl/auth/login",
+          data: {"username": username, "password": password});
+
+      return response.data;
+    } on DioError catch (e) {
       //returns the error object if there is any.
       return e.response!.data;
     }
   }
 
   Future<Response> Register() async {}
+  Future<Response> Logout() async {}
+  Future<Response> Refresh() async {}
 }
