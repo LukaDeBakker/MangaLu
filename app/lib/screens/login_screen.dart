7a9384ed167e9d45e61d9c780afbea3950677cc1
Luka De Bakker <lukadebakker5@gmail.com>
2022-11-11 21:11:21 +0100

    ✨ Learning how dart UI works

--- app/lib/screens/login_screen.dart
+++ app/lib/screens/login_screen.dart
@@ -10,8 +10,34 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      body: Form(
+        key: _formKey,
+        child: Stack(children: [
+          SizedBox(
+            width: size.width,
+            height: size.height,
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: size.width * 0.85,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
